d19b2e9e4c1b354c235e83aa9af8e6c9272a32a6
dar3v <un.finished342@slmail.me>
2025-12-04 02:09:05 +0800

    more global fields

--- Scripts/Globals.cs
+++ Scripts/Globals.cs
@@ -2,7 +2,30 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public static class Globals
+public partial class Globals : Node
 {
-    public static List<GvObject> GvObjects;
+    public const float G = 0.1f; // Gravitational constant
+    public const float SchwarzG = 1.0f; // Schwarzschild
+    public const float CSquared = 1.0f; // Speed of light squared
+    
+    private static readonly List<GvObject> _gvObjects = [];
+
+    public static void AddGvObject(GvObject gvObject)
+    {
+        if (!_gvObjects.Contains(gvObject))
+        {
+            _gvObjects.Add(gvObject);
+            GD.Print($"Registered GvObject: {gvObject.Name}");
+        }
+    }
+
+    public static void RmGvObject(GvObject gvObject)
+    {
+        _gvObjects.Remove(gvObject);
+    }
+
+    public static List<GvObject> GetAllGvObjects()
+    {
+        return _gvObjects;
+    }
 }
