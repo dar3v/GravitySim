0c4747f14e6c203cad66f06204b7a693c8783264
dar3v <un.finished342@slmail.me>
2025-12-06 02:04:15 +0800

    update GvObject attributes

--- Scripts/ObjectScripts/GvObject.cs
+++ Scripts/ObjectScripts/GvObject.cs
@@ -5,18 +5,27 @@ using Godot;
 // uses this script for movement and gravitational influence.
 public partial class GvObject : Node3D
 {
-    [Export] public float Mass { get; set; } = 1.0f; // Defines gravitational strength (small for satellites, large for suns)
+    [Export] public float Mass { get; set; } = 1.0f;
+    [Export] public float Radius { get; set; } = 1.0f;
     [Export] public Vector3 InitialVelocity = Vector3.Zero; 
     
+    // public float SchwarzschildRadius { get; private set; } = 0.0f;
     public Vector3 Velocity = Vector3.Zero;
-    private float _G = Globals.G;
-    public float SchwarzschildRadius { get; private set; } = 0.0f;
+    private MeshInstance3D _meshInstance;  // Reference to MeshInstance3D
 
     public override void _Ready()
     {
         // r_s = 2GM / c^2
         // SchwarzschildRadius = 2.0f * Globals.G * Mass / Globals.CSquared;
-        
+
+        // Assuming the MeshInstance3D is the first child of GvObject
+        // NOTE: this will bug out if MeshInstance is not the first child of this Object
+        _meshInstance = GetChild<MeshInstance3D>(0);
+        if (_meshInstance is not null)
+        {
+            _meshInstance.Scale = new Vector3(Radius, Radius, Radius);
+        }
+
         Velocity = InitialVelocity;
         Globals.AddGvObject(this);
     }
